6e9da4b6a297f1d6f474d3c4a2afd45f28414746
Gerard Yap <cheekian.yap@aiesec.net>
2022-03-14 20:26:22 +0800

    add web scraping

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Yapcheekian/blackhat-go
 go 1.17
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/google/gopacket v1.1.19
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
@@ -10,7 +11,9 @@ require (
 )
 
 require (
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ web_scraping/goquery/hyperlink/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	// Load command line arguments
+	if len(os.Args) != 2 {
+		fmt.Println("Find all links in a web page")
+		fmt.Println("Usage: " + os.Args[0] + " <url>")
+		fmt.Println("Example: " + os.Args[0] +
+			" https://www.devdungeon.com")
+		os.Exit(1)
+	}
+	url := os.Args[1]
+
+	// Fetch the URL
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Error fetching URL. ", err)
+	}
+
+	// Extract all links
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body. ", err)
+	}
+
+	// Find and print all links
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		href, exists := s.Attr("href")
+		if exists {
+			fmt.Println(href)
+		}
+	})
+}
